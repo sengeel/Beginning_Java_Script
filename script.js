87fb5c00bdbf77415d3d4129e3d9a1b1feb55747
sengeel <okereolujiechizurum@gmail.com>
2023-08-21 11:22:03 +0100

    Beginning Java Script

---
+++ script.js
@@ -0,0 +1,44 @@
+// const name = "noble";
+// console.log(name);
+
+// let surname = "Amadi";
+// console.log(surname);
+
+// const name = "Chizurum";
+// console.log(name);
+
+// const surname = "Okere-Olujie";
+// console.log(surname);
+
+// const middlename = "Elvis";
+// console.log(middlename)
+
+console.log("Breakfast Requirement");
+
+let prod1 = 'Bread';
+let price1 = 10;
+
+let qty1 = 3;
+
+let answer = price1 * qty1;
+console.log(prod1, ":$" + price1, "x", qty1, " = $" + answer )
+
+let prod2 = 'Milk';
+let price2 = 8;
+
+let qty2 = 6;
+
+let sum = price2 * qty2;
+console.log(prod2, ":$" + price2, "x", qty2, " = $" + sum)
+
+let prod3 = 'Milo';
+let price3 = 12;
+
+let qty3 = 6;
+
+let total = price3 * qty3;
+console.log(prod3, ":$" + price3, "x", qty3, " = $" + total)
+
+let overall = answer + sum + total;
+console.log( "Total Cost: $" + answer, "+ $"  + sum, "+ $"  + total, " = $" + overall)
+
